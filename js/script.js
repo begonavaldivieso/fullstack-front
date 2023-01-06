cebe6e9ca024c61fd79bc627a51995cd4ba9d547
begonavaldivieso <begona.valdivieso@getronics.com>
2023-01-06 17:10:55 +0100

    Función checksize

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
-function iniciar()
-{
-    document.getElementById('pedidos_form').addEventListener('submit',validar);
-    document.getElementById('nombre').addEventListener('focus', limpiar_fondo);
-    document.getElementById('apellidos').addEventListener('focus', limpiar_fondo);
-}
-function validar(evento)
-{ 
-    if (document.getElementById("nombre").value.trim() == '')
-    {        
-        document.getElementById("l_nombre").style.color="#ff0000";
-        evento.preventDefault();
-    }
-    if (document.getElementById("apellidos").value.trim() == '')
-    {
-        document.getElementById("l_apellidos").style.color="#ff0000";
-        evento.preventDefault();
-    }
-}
+$(function() {
+      
+    $("#tamano").on('change', function() {
+      valor = this.value
+      // Aqui va la llamada AJAX con JQuery
+      
+      request = $.ajax({
+        type: "POST",
+        url: "http://localhost:5000/checksize",
+        data: {tamano: this.value},
+        success : function(result) {
+            $("#resultado_tamano").html(result);  
+        }            
+      })
+ 
+    });
 
-function limpiar_fondo(evento)
-{
-    document.getElementById("l_"+ evento.target.id).style.color="#000000";
-}
+    $("#pedidos_form").submit(function (e) 
+    {
+        if($.trim($("#nombre").val()) == '' )
+        {
+          $("#l_nombre").css("color","#ff0000");
+          e.preventDefault();
+        }
+        else {
+          $("#l_nombre").css("color","#000000");
+        }
+        if($.trim($("#apellidos").val()) == '' )
+        {
+          $("#l_apellidos").css("color","#ff0000");
+          e.preventDefault();
+        }
+        else {
+          $("#l_apellidos").css("color","#000000");
+        }
+    });
+
+    
+  });
